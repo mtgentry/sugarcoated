e8ff87f73086e8881be52288d3896b23f28a6c9e
mtgentry <mgentry@masongentry.com>
2024-12-05 11:47:02 -0800

    changed node setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,19 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  // Enable server-side rendering
   ssr: true,
 
-  // Target: https://go.nuxtjs.dev/config-target
+  // Target: server
   target: 'server',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  // Server Configuration
+  server: {
+    port: process.env.PORT || 3000, // Listen on the PORT environment variable
+    host: '0.0.0.0' // Listen on all network interfaces
+  },
+
+  // Global page headers
   head: {
     titleTemplate: process.env.IS_AGENCY === '1' ? 'Sugarcoated' : 'Sugarcoated',
     htmlAttrs: {
@@ -18,76 +24,26 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
-      { name: 'theme-color', content: "#C1C1C1"}
+      { name: 'theme-color', content: "#C1C1C1" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  // Global CSS
+  css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  // Plugins to run before rendering page
   plugins: [
     { src: "@/plugins/aos", mode: "client" },
     '@/plugins/vue-gtag'
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  // Auto import components
   components: true,
+
+  // Environment Variables
   env: {
     IS_AGENCY: process.env.IS_AGENCY === '1'
-  },
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxtjs/vuetify',
-    '@nuxtjs/axios',
-    'nuxt-compress'
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // ...other modules you might already have
-  ],
-
-  // Server Middleware: Add this block to register your middleware
-  // serverMiddleware: [
-  //  '~/middleware/pdfProxy',  // Add this line
-  // ],
-
-  axios: {
-    baseURL: `${process.env.BASE_URL}/domains/${process.env.IS_AGENCY === '1' ? 'agency' : 'portfolio'}`
-  },
-
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
-  },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    // extend(config, { isClient }) {
-    //   // Extend only webpack config for client-bundle
-    //   if (isClient) {
-    //     config.devtool = 'source-map'
-    //   }
-    // }
-  }
-}
+ 
